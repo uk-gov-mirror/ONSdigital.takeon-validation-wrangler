75b996044c06910e5d0fd64743c5a01ec4b6dabf
ranjit-das <Ranjit.Das@ons.gov.uk>
2019-11-18 16:26:19 +0000

    LU-4024: utils.go added to support testing

---
+++ utils.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"reflect"
+)
+
+func Equal(vx, vy interface{}) bool {
+	if reflect.TypeOf(vx) != reflect.TypeOf(vy) {
+		return false
+	}
+
+	switch x := vx.(type) {
+	case map[string]interface{}:
+		y := vy.(map[string]interface{})
+
+		if len(x) != len(y) {
+			return false
+		}
+
+		for k, v := range x {
+			val2 := y[k]
+
+			if (v == nil) != (val2 == nil) {
+				return false
+			}
+
+			if !Equal(v, val2) {
+				return false
+			}
+		}
+
+		return true
+	case []interface{}:
+		y := vy.([]interface{})
+
+		if len(x) != len(y) {
+			return false
+		}
+
+		var matches int
+		flagged := make([]bool, len(y))
+		for _, v := range x {
+			for i, v2 := range y {
+				if Equal(v, v2) && !flagged[i] {
+					matches++
+					flagged[i] = true
+					break
+				}
+			}
+		}
+		return matches == len(x)
+	default:
+		return vx == vy
+	}
+}
+
+// Unmarshal parses the Body-encoded data into an interface{}
+func Unmarshal(b []byte) (interface{}, error) {
+	var j interface{}
+
+	err := json.Unmarshal(b, &j)
+	if err != nil {
+		return nil, err
+	}
+
+	return j, nil
+}
